8f940937df475d5e1ac2d877f456ecef64e848aa
luyaj <1579618152@qq.com>
2018-01-28 21:14:52 +0800

    增加管理员权限role

--- luya_movie/schemas/user.js
+++ luya_movie/schemas/user.js
@@ -8,6 +8,13 @@ const UserSchema = new Schema({
     type: String
   },
   password: String,
+  // 0: nomal user
+  // >10: admin
+  // >50: super admin
+  role: {
+    type: Number,
+    default: 0
+  },
   meta: {
     createAt: {
       type: Date,
