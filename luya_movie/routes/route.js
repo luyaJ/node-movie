7ee98d025b1f318c6ac5f9bda4078381d4b36eb3
luyaj <1579618152@qq.com>
2018-01-28 21:15:31 +0800

    增加管理员权限路由

--- luya_movie/routes/route.js
+++ luya_movie/routes/route.js
@@ -19,15 +19,15 @@ module.exports = function (app) {
   app.get('/logout', User.logout);
   app.post('/user/register', User.register);
   app.post('/user/login', User.login);
-  app.get('/admin/userlist', User.userList);
+  app.get('/admin/userlist', User.loginRequired, User.adminRequired, User.userList);
 
   // Movie
   app.get('/movie/:id', Movie.detail);
-  app.get('/admin/movienew', Movie.new);
-  app.get('/admin/update/:id', Movie.update);
-  app.post('/admin/movie', Movie.save);
-  app.get('/admin/movielist', Movie.movieList);
-  app.delete('/admin/movielist', Movie.delete)
+  app.get('/admin/movienew', User.loginRequired, User.adminRequired, Movie.new);
+  app.get('/admin/update/:id', User.loginRequired, User.adminRequired, Movie.update);
+  app.post('/admin/movie', User.loginRequired, User.adminRequired, Movie.save);
+  app.get('/admin/movielist', User.loginRequired, User.adminRequired, Movie.movieList);
+  app.delete('/admin/movielist', User.loginRequired, User.adminRequired, Movie.delete)
 
 
 
