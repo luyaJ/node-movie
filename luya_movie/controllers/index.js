7511006b92af3c6e86a65c83979ad5a570ae4714
luyaj <1579618152@qq.com>
2018-02-01 22:35:25 +0800

    1增加搜索电影的功能2整理semantic-ui的引入

--- luya_movie/controllers/index.js
+++ luya_movie/controllers/index.js
@@ -7,6 +7,7 @@ exports.index = function (req, res) {
     .find({})
     .populate({
       path: 'movies',
+      select: 'title poster',
       options: {
         limit: 5
       }
@@ -25,11 +26,14 @@ exports.index = function (req, res) {
 // search page
 exports.search = function (req, res) {
   const catId = req.query.cat;
-  const page = req.query.p;
+  const q = req.query.q;
+  const page = parseInt(req.query.p, 10) || 0;
   const count = 2;
   const index = page * count;
 
-  Category
+  //先看有没有catId
+  if(catId){
+    Category
     .find({_id: catId})
     .populate({
       path: 'movies',
@@ -51,5 +55,27 @@ exports.search = function (req, res) {
         totalPage: Math.ceil(movies.length / count), //向上舍入 当该页面只有单数条电影时（如1部电影是1/2=0.5）就是1了
         movies: results,
       });
+    });
+  } else { 
+    // 通过搜索框来找
+    Movie
+    .find({title: new RegExp(q+'.*', 'i')})
+    .exec(function(err, movies){
+      if(err){
+        console.log(err);
+      }
+      const results = movies.slice(index, index + count);
+
+      res.render('results', {
+        title: "结果列表页面",
+        keyword: q,
+        currentPage: page + 1, // 参数是从0开始的 所以加1
+        query: 'q=' + q,
+        totalPage: Math.ceil(movies.length / count), //向上舍入 当该页面只有单数条电影时（如1部电影是1/2=0.5）就是1了
+        movies: results,
+      });
     })
+  }
+
+  
 }
